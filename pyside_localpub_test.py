b42344089ea63c95396aa661fa1f6aac6250cb63
DESKTOP-RGFHS7D\simo <simo@c2monster.com>
2017-07-21 18:44:37 +0900

    coding start..

--- pyside_localpub_test.py
+++ pyside_localpub_test.py
@@ -46,7 +46,27 @@ taskthumbnail = u"C:\\Users\\simo\\Pictures\\이미지\\thumbnail_한글 - 75px.
 
 # print os.path.abspath('')
 
+def gettaskinfo(env):
+    shotname = u''
+    wh = whAPI.Get(corpPrefix=env.Company, url=env.ServerName)
+    pprint(wh.ContactList(projectId=env.Project,shotId=env.ShotName))
 
+    for shots in (wh.ShotNames(projectId=env.Project,seqId=env.SeqName)['shotList']):
+        if shots['shotId'] == env.ShotName:
+            shotname = unicode(shots['shotNm'])
+    for seqs in wh.Seqnames(projectId=env.Project)['sequenceList']:
+        if seqs['sequenceId'] == env.SeqName:
+            seqname = unicode(seqs['sequenceNm'])
+            print seqname , env.SeqName
+    if env.DirType == 'shot':
+        env.__setattr__('SeqId',env.SeqName)
+        env.__setattr__('SeqName',seqname)
+        env.__setattr__('ShotId',env.ShotName)
+        env.__setattr__('ShotName',shotname)
+    env.__setattr__('ProjectName','projectname')
+    # env.__setattr__('UserName',username)
+    # pprint(env.__dict__)
+    return env
 
 class LocalPub(QWidget):
     def __init__(self,parent=None):
